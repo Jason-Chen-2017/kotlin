15ecb14cf8ede58f3717b3475d6f95a7ae7b25dd
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-02-28 17:24:13 +0300

    KtLightMethod(Field): use dummyDelegate to determine modifier properties

---
+++ compiler/testData/asJava/lightClasses/AnnotationModifiers.java
@@ -0,0 +1,13 @@
+public final class A {
+    @kotlin.jvm.Transient
+    @kotlin.jvm.Volatile
+    private final transient volatile int c = 3;
+
+    @kotlin.jvm.Synchronized
+    @kotlin.jvm.Strictfp
+    public final synchronized strictfp void f() { /* compiled code */ }
+
+    public final int getC() { /* compiled code */ }
+
+    public A() { /* compiled code */ }
+}
---
+++ compiler/testData/asJava/lightClasses/AnnotationModifiers.kt
@@ -0,0 +1,13 @@
+// A
+
+class A {
+    @Synchronized
+    @Strictfp
+    fun f() {
+
+    }
+
+    @Transient
+    @Volatile
+    val c = 3
+}
